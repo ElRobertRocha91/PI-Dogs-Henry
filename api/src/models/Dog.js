4c1325ced959f97a8ba30b9bbe73910a7452cc05
elrobertrocha91 <robertoestebanrocha91@gmail.com>
2023-07-11 21:27:14 -0300

    adding data base

--- api/src/models/Dog.js
+++ api/src/models/Dog.js
@@ -4,9 +4,46 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('dog', {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      allowNull: false,
+      primaryKey : true,
+    },
     name: {
+      type: DataTypes.STRING,
+      allowNull: false,//Campo obligartorio
+    },
+    heightMax: {//Altura Max
       type: DataTypes.STRING,
       allowNull: false,
     },
+    heightMin:{
+      type:DataTypes.STRING,
+      allowNull:false
+    },
+    weightMax: {//Peso Max
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    weightMin: {
+      type :DataTypes.STRING,
+      allowNull: false
+    },
+    life_span: {//Esperanza de vida
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    image: {
+      type: DataTypes.TEXT,
+      allowNull: false
+    },
+    createInDb: {
+      type: DataTypes.BOOLEAN,
+      allowNull:false,
+      defaultValue: true,
+    }
+  },{
+    timestamps: false
   });
 };
