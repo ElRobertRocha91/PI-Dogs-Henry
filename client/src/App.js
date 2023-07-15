91a55730f5b59cc79e8a75f6ee8fab9e664aaff3
elrobertrocha91 <robertoestebanrocha91@gmail.com>
2023-07-15 20:59:13 -0300

    adding react, redux and axios

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,21 @@
 import './App.css';
+//me traigo:
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import LandingPage from './components/LandingPage';
+import Home from './components/Home';
+
+//Envolvemos con el browserRuter todo el return de app
 
 function App() {
   return (
+    <BrowserRouter>
     <div className="App">
-      <h1>Henry Dogs</h1>
+      <Switch>
+        <Route exact path= '/' component= {LandingPage}/>
+        <Route path= '/home' component={Home}/>
+      </Switch>
     </div>
+    </BrowserRouter>
   );
 }
 
--- client/src/index.js
+++ client/src/index.js
@@ -3,12 +3,18 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+//Ahora le pasamos el provider para pasar el store:
+import { Provider } from 'react-redux';
+import { store } from './store';
 
+//"Super importante envolver mi archivo raiz en un provider para que redux lo detecte"
 ReactDOM.render(
+  <Provider store={store}>
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
+  </Provider>
+  ,  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
